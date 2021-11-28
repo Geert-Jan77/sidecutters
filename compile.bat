719e6effac8bef923f9051e8fe4559723f145375
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-11-28 05:33:05 +0100

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 @echo off
 gcc -v 1> gcc 2>&1
+rem This creates a file called gcc containing the output of gcc -v, and we will delete it later.
 find gcc "gcc version"
 find gcc "gcc version 11.2.0" && echo gcc version 11.2.0 found. || echo Compiler gcc version 11.2.0 not found. && goto Einde
 @echo on
