ababaf398779aee001c02d2298820fe7c6a0a632
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-11-28 04:26:55 +0100

    Add files via upload

---
+++ src/side cutters.c
@@ -0,0 +1,91 @@
+// compile 
+// windres icon.rc -O coff -o icon.res  
+// windres info.rc -O coff -o info.res  
+// gcc "side cutters.c" icon.res info.res -o "side cutters.exe"
+
+#include <windows.h>
+#include "../rsc/resource.h"
+
+const char g_szClassName[] = "myWindowClass";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_LBUTTONDOWN:
+        {
+            char szFileName[MAX_PATH];
+            HINSTANCE hInstance = GetModuleHandle(NULL);
+
+            GetModuleFileName(hInstance, szFileName, MAX_PATH);
+            MessageBox(hwnd, szFileName, "This program is:", MB_OK | MB_ICONINFORMATION);
+        }
+        break;
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	int iHsize = GetSystemMetrics(SM_CXSCREEN);
+	int iVsize = GetSystemMetrics(SM_CYSCREEN);
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszClassName = g_szClassName;
+	wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
+    wc.hIcon  = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
+    wc.hIconSm  = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "Side Cutters",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, iHsize, iVsize,
+        NULL, NULL, hInstance, NULL);
+
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
+}
