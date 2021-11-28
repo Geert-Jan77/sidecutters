8489f3d80583d3d2bc2ca33332731c4060a51c71
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-11-28 05:27:58 +0100

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,5 +1,13 @@
+@echo off
+gcc -v 1> gcc 2>&1
+find gcc "gcc version"
+find gcc "gcc version 11.2.0" && echo gcc version 11.2.0 found. || echo Compiler gcc version 11.2.0 not found. && goto Einde
+@echo on
 gcc src/test/testpdf.c -o testpdf.exe
 windres rsc/icon.rc -O coff -o rsc/icon.res  
 windres rsc/info.rc -O coff -o rsc/info.res  
 gcc "src/side cutters.c" rsc/icon.res rsc/info.res -o "side cutters.exe"
+:Einde
+@echo off
+del gcc
 pause
