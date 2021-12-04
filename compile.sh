4e1b8fcca3866f40dd6ab801ec42ea1d6f942dc3
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-04 19:03:30 +0100

    Add files via upload
    Add MacOS files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 gcc --version
-gcc -lobjc -o Side-Cutters src/*.m -framework cocoa
-echo Run: ./Side-Cutters
+gcc -lobjc -o "Side Cutters.app/Contents/MacOS/Side-Cutters" src/*.m -framework cocoa
+chmod +x "Side Cutters.app"
+echo Run: Double click Side Cutters app
