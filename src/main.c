8c2267d7760e876674cb1396b7314b9d32cfd97e
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-08 03:04:58 +0100

    This is the MacOS Entry point

--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
 /*
- This is the Entry point for MacOS.
-
  Compile
 
     gcc main.c -o main `pkg-config --cflags --libs gtk+-3.0`
@@ -10,50 +8,24 @@
     ./main
 */
 
-
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+static void activate (GtkApplication *app, gpointer user_data)
 {
-  g_print ("Hello World\n");
+    GtkWidget *window;
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Side Cutters");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all (window);
 }
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+int main (int argc, char **argv)
 {
-  GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Side Cutters");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
-
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  // g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
-
-  gtk_widget_show_all (window);
-}
-
-int
-main (int    argc,
-      char **argv)
-{
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("side-cutters", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
+    GtkApplication *app;
+    int status;
+    app = gtk_application_new ("side.cutters", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+    return status;
 }
