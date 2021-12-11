e30b660c65b949138cbebc879b5032901ccf0933
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-12 00:16:40 +0100

    Add files via upload

---
+++ compile-MSwin.sh
@@ -0,0 +1,5 @@
+gcc --version
+gcc src/main.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
+echo Run: Double click 'side cutters.exe' - MS Windows executable 
+echo Press any key. 
+read
---
+++ compile-MacOS.sh
@@ -0,0 +1,7 @@
+gcc --version
+mkdir "Side Cutters.app/Contents/MacOS"
+gcc src/main.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
+chmod +x "Side Cutters.app"
+echo Run: Double click 'Side Cutters.app' - Apple MacOS application
+echo Press any key. 
+read
