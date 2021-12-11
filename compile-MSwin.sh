382c1091f4c04f0df09c6d7d85c82fe22a452a01
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-12 00:34:55 +0100

    Update compile-MSwin.sh

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -1,3 +1,4 @@
+# This bashfile will run on Windows in an MSYS console.
 gcc --version
 gcc src/main.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
 echo Run: Double click 'side cutters.exe' - MS Windows executable 
