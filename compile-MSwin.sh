a7d88e9f7129a9a4f034b11a68e7587dcc025430
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-12 02:04:00 +0100

    Add files via upload

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -1,6 +1,5 @@
-# This bashfile will run on Windows in an MSYS console.
 gcc --version
-gcc src/main.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
+gcc src/main-win.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
 echo Run: Double click 'side cutters.exe' - MS Windows executable 
 echo Press any key. 
 read
