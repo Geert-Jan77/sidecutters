33c67b2e18e14adc599ba7ed886d91d72dd8b54d
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-13 23:28:01 +0100

    Add files via upload

--- compile-MacOS.sh
+++ compile-MacOS.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-gcc --version
+gcc --version | grep gcc | awk '{print $1" "$5}'
+gcc --version | awk '/gcc/ && ($5+0)==11.2{print "Version is correct"}'
 mkdir "Side Cutters.app/Contents/MacOS"
 cp -R rsc "Side Cutters.app/Contents/Resources"
-filename="src/main.c"
+filename="src/main_.c"
+cp src/main.c $filename
 search="rsc/"
 replace=$(pwd)
 echo "I'm going to replace '${search}' with '${replace}/Side Cutters.app/Contents/Resources/rsc/' in '${filename}'"
 if [[ $search != "" && $replace != "" ]]; then
 sed -i "" "s|${search}|${replace}/Side Cutters.app/Contents/Resources/rsc/|" $filename
 fi
-gcc src/main.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
+gcc src/main_.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
 chmod +x "Side Cutters.app"
 echo Run: Double click 'Side Cutters.app' - Apple MacOS application
 echo Press any key. 
