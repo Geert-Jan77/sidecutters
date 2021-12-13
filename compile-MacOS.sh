82fe4af868fb390e673fe12283af9957a7eb71a6
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-13 19:48:23 +0100

    Update compile-MacOS.sh

--- compile-MacOS.sh
+++ compile-MacOS.sh
@@ -1,5 +1,6 @@
 gcc --version
 mkdir "Side Cutters.app/Contents/MacOS"
+cp -R rsc "Side Cutters.app/Contents/MacOS/rsc"
 gcc src/main.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
 chmod +x "Side Cutters.app"
 echo Run: Double click 'Side Cutters.app' - Apple MacOS application
