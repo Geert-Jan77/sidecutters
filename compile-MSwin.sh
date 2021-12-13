862e777b03a50b36bd685c9ddba7e49d67d621f0
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-14 00:09:02 +0100

    Add files via upload

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -1,5 +1,6 @@
 # This bash script runs on MS Windows in an MSYS shell. 
-gcc --version
+gcc --version | grep gcc | awk '{print $1" "$7}'
+gcc --version | awk '/gcc/ && ($7+0)==11.2{print "Version is correct"}'
 gcc src/main.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
 echo Run: Double click 'side cutters.exe' - MS Windows executable 
 echo Press any key. 
