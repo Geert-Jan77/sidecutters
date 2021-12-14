3ca4e9ce44aee7d6764966ccb24b689551da4a2d
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-14 02:54:58 +0100

    Update compile-MSwin.sh

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -4,7 +4,6 @@ gcc --version | awk '/gcc/ && ($7+0)==11.2{print "Version is correct"}'
 windres -i rsc/icon.rc -O coff -o rsc/icon.o
 windres -i rsc/info.rc -O coff -o rsc/info.o
 gcc src/main.c rsc/icon.o rsc/info.o -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
-#gcc src/main.c -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
 echo Run: Double click 'side cutters.exe' - MS Windows executable 
 echo Press any key. 
 read
