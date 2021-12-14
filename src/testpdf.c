d92ba0076d9eb6607724d68622be26cb3b6a3dbd
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-14 17:53:17 +0100

    Add files via upload

--- src/testpdf.c
+++ src/testpdf.c
@@ -8,9 +8,35 @@
 	 
 -----------------------------------------------------------------------------------------
 */
- 
-#include "pdf.h"
-#include "pdf.c"
+
+#include "pdfgen.h"
+#include "pdfgen.c"
+
+int testpdf(char *argv[])
+{
+    struct pdf_info info = {
+        .creator = "My software",
+        .producer = "My software",
+        .title = "My document",
+        .author = "My name",
+        .subject = "My subject",
+        .date = "Today"
+    };
+    struct pdf_doc *pdf = pdf_create(PDF_A4_WIDTH, PDF_A4_HEIGHT, &info);
+    //pdf_set_font(pdf, "Times-Roman");
+    pdf_append_page(pdf);
+    //pdf_add_text(pdf, NULL, "This is text", 12, 50, 20, PDF_BLACK);
+    pdf_add_line(pdf, NULL, 50, 24, 150, 24, 3, PDF_BLACK);
+    pdf_save(pdf, "/Users/geert-januijtdewilligen/Downloads/sidecutters-main/output.pdf");
+    pdf_destroy(pdf);
+    return 0;
+}
+
+/*
+
+
+#include "pdfgen.h"
+#include "pdfgen.c"
  
 int testpdf(char *argv[])
 {
@@ -28,7 +54,7 @@ int testpdf(char *argv[])
 	
 	printf("Export Pdf %s\n", argv[0]);
 	
-/*		Todo			
+/		Todo
     pdf_add_circle(pdf, NULL, 100, 240, 50, 5, PDF_RGB(0xff, 0, 0),
                    PDF_TRANSPARENT);
     pdf_add_ellipse(pdf, NULL, 100, 240, 40, 30, 2, PDF_RGB(0xff, 0xff, 0),
@@ -45,14 +71,14 @@ int testpdf(char *argv[])
     float p2Y[] = {400, 500, 400, 500};
     pdf_add_filled_polygon(pdf, NULL, p2X, p2Y, 4, 4,
                            PDF_RGB(0xff, 0x77, 0x77));
-*/
+/
 
     
 	
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 1 Quadratic Bezier Curve");
 	pdf_page_set_size(pdf, NULL, PDF_A4_HEIGHT, PDF_A4_WIDTH);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "y = 20", 12, 50, 26, PDF_BLACK);
 	pdf_add_line(pdf, NULL, 50, 20, 150, 20, 0.3, PDF_BLACK);
 	pdf_add_circle(pdf, NULL, 500, 20, 2, 0.3, PDF_BLACK, PDF_WHITE); // x = 500 y = 20 radius 2 width 0.3
@@ -67,7 +93,7 @@ int testpdf(char *argv[])
     pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 2 Cubic Bezier Curve");
 	pdf_page_set_size(pdf, NULL, PDF_A4_HEIGHT, PDF_A4_WIDTH);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "y = 20", 12, 50, 26, PDF_BLACK);
 	pdf_add_line(pdf, NULL, 50, 20, 150, 20, 0.3, PDF_BLACK);
 	pdf_add_circle(pdf, NULL, 500, 20, 2, 0.3, PDF_BLACK, PDF_WHITE); // x = 500 y = 20 radius 2 width 0.3
@@ -84,7 +110,7 @@ int testpdf(char *argv[])
 	
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 3 Quadratic Bezier Curve");
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "y = 20", 12, 50, 26, PDF_BLACK);
 	pdf_add_line(pdf, NULL, 50, 20, 150, 20, 0.3, PDF_BLACK);
 	pdf_add_circle(pdf, NULL, 500, 20, 2, 0.3, PDF_BLACK, PDF_WHITE); // x = 500 y = 20 radius 2 width 0.3
@@ -134,7 +160,7 @@ int testpdf(char *argv[])
 	pdf_add_bookmark(pdf, NULL, -1, "Page 7 TextBoxes in all supported fonts");
 	fontheight = 10;
 	textblockposition = 800;
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "Calibri 10 pt.", fontheight, 50, textblockposition -  fontheight, PDF_BLACK);
 	pdf_set_font(pdf, "Helvetica");
 	pdf_add_text(pdf, NULL, "Helvetica", fontheight, 50, textblockposition - 2 * fontheight, PDF_BLACK);
@@ -163,7 +189,7 @@ int testpdf(char *argv[])
     
 	fontheight = 12;
 	textblockposition = 660;
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "Calibri 12 pt.", fontheight, 50, textblockposition -  fontheight, PDF_BLACK);
 	pdf_set_font(pdf, "Helvetica");
 	pdf_add_text(pdf, NULL, "Helvetica", fontheight, 50, textblockposition - 2 * fontheight, PDF_BLACK);
@@ -192,7 +218,7 @@ int testpdf(char *argv[])
 	
 	fontheight = 15.6;
 	textblockposition = 492;
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 	pdf_add_text(pdf, NULL, "Calibri 15.6 pt.", fontheight, 50, textblockposition -  fontheight, PDF_BLACK);
 	pdf_set_font(pdf, "Helvetica");
 	pdf_add_text(pdf, NULL, "Helvetica", fontheight, 50, textblockposition - 2 * fontheight, PDF_BLACK);
@@ -221,9 +247,9 @@ int testpdf(char *argv[])
 	
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 8 Image, Barcode, Line");
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
 
-    pdf_add_image_file(pdf, NULL, 150, 50, 200, 200, "rsc/side cutters.jpg"); // 995 * 996
+    pdf_add_image_file(pdf, NULL, 150, 50, 200, 200, "/Users/geert-januijtdewilligen/Downloads/sidecutters-main/Side Cutters.app/Contents/Resources/rsc/side cutters.jpg"); // 995 * 996
     pdf_add_barcode(pdf, NULL, PDF_BARCODE_128A, 50, 300, 200, 50, "Code128", PDF_RGB(0, 0, 0));
     pdf_add_barcode(pdf, NULL, PDF_BARCODE_39, 50, 400, 400, 50, "CODE39", PDF_RGB(0, 0, 0));
     pdf_add_text(pdf, NULL, "Page 8", 10, 20, 30, PDF_RGB(0, 0, 0));
@@ -258,7 +284,7 @@ int testpdf(char *argv[])
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 11 Text Wrapping Calibri");
 	pdf_page_set_size(pdf, NULL, PDF_A4_HEIGHT, PDF_A4_WIDTH);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
 		"space                       space                       space                       space                       space\n"
@@ -269,7 +295,7 @@ int testpdf(char *argv[])
         16, 60, 400, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 58, 400 + 16 - 2, 304, -height - 2, 2, PDF_RGB(0, 0, 0));
 	
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
 		"space                       space                       space                       space                       space\n"
@@ -284,7 +310,7 @@ int testpdf(char *argv[])
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 12 Text Wrapping Calibri");
 	pdf_page_set_size(pdf, NULL, PDF_A3_WIDTH, PDF_A3_HEIGHT);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n"
@@ -316,7 +342,7 @@ int testpdf(char *argv[])
         16, 60, 1100, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 58, 1100 + 16 - 2, 304, -height - 2, 2, PDF_RGB(0, 0, 0));
 	
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n"
@@ -351,7 +377,7 @@ int testpdf(char *argv[])
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 13 Text Wrapping Calibri");
 	pdf_page_set_size(pdf, NULL, PDF_A3_WIDTH, PDF_A3_HEIGHT);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n"
@@ -383,7 +409,7 @@ int testpdf(char *argv[])
         16, 60, 1100, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 58, 1100 + 16 - 2, 304, -height - 2, 2, PDF_RGB(0, 0, 0));
 	
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n"
@@ -419,7 +445,7 @@ int testpdf(char *argv[])
 	// other ascii 0-127 characters: !"#$%&'()*+,-./0123456789:;<=>?@
 	pdf_add_bookmark(pdf, NULL, -1, "Page 14 Text Wrapping Calibri");
 	pdf_page_set_size(pdf, NULL, PDF_A3_WIDTH, PDF_A3_HEIGHT);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
@@ -458,7 +484,7 @@ int testpdf(char *argv[])
         16, 60, 1100, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 58, 1100 + 16 - 2, 304, -height - 2, 2, PDF_RGB(0, 0, 0));
 	
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
@@ -501,7 +527,7 @@ int testpdf(char *argv[])
 	pdf_append_page(pdf);
 	pdf_add_bookmark(pdf, NULL, -1, "Page 15 Text Wrapping Calibri");
 	pdf_page_set_size(pdf, NULL, PDF_A3_WIDTH, PDF_A3_HEIGHT);
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[["
@@ -517,7 +543,7 @@ int testpdf(char *argv[])
         16, 60, 1100, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 58, 1100 + 16 - 2, 304, -height - 2, 2, PDF_RGB(0, 0, 0));
 	
-	pdf_set_font(pdf, "Calibri");
+	//pdf_set_font(pdf, "Calibri");
     pdf_add_text_wrap(
         pdf, NULL,
         "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[["
@@ -533,7 +559,7 @@ int testpdf(char *argv[])
         12, 460, 1100, PDF_RGB(0, 0, 0), 300, PDF_ALIGN_JUSTIFY, &height);
     pdf_add_rectangle(pdf, NULL, 458, 1100 + 12 - 2, 304, -height - 2 , 2, PDF_RGB(0, 0, 0));
 	
-    pdf_save(pdf, "testpdf.pdf");
+    pdf_save(pdf, "/Users/geert-januijtdewilligen/Downloads/sidecutters-main/testpdf.pdf");
     const char *err_str = pdf_get_err(pdf, &err);
     if (err_str) {
         fprintf(stderr, "PDF Error: %d - %s\n", err, err_str);
@@ -544,3 +570,5 @@ int testpdf(char *argv[])
     
     return 0;
 }
+
+*/
