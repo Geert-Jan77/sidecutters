fc90e1d3216a8a220b65193fdc1c5ae1662a0985
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-16 02:11:37 +0100

    Add files via upload

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -1,4 +1,26 @@
 # This bash script runs on MS Windows in an MSYS shell. 
+
+# Find the documents folder
+@echo off
+for /f "skip=2 tokens=2*" %%c in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do @set "Documents=%%d" && echo WIN XP - 10
+# xcopy "C:\test.txt" "%docs%" /f /y
+echo %docs%
+
+Echo current users desktop
+for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop"') do @set "Desktop=%%d"
+
+Echo current users documents
+for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do @set "Documents2=%%d"
+
+Echo current user start menu
+for /f "skip=2 tokens=3*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Start Menu"') do @set "Start=%%d"
+
+Echo current user startup
+for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Startup"') do @set "Startup=%%d"
+
+Echo current users pictures
+for /f "skip=2 tokens=3*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "My Pictures"') do @set "Pictures=%%d"
+
 gcc --version | grep gcc | awk '{print $1" "$7}'
 gcc --version | awk '/gcc/ && ($7+0)==11.2{print "Version is correct"}'
 windres -i rsc/icon.rc -O coff -o rsc/icon.o
--- compile-MacOS.sh
+++ compile-MacOS.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 gcc --version | grep gcc | awk '{print $1" "$5}'
 gcc --version | awk '/gcc/ && ($5+0)==11.2{print "Version is correct"}'
+if [ -d "~/.Trash" ]; then thrash="~/.Trash"
+fi
 mkdir "Side Cutters.app/Contents/MacOS"
 cp -R rsc "Side Cutters.app/Contents/Resources"
-filename="src/main_.c"
-cp src/main.c $filename
+filename="src/config"
 search="rsc/"
 replace=$(pwd)
 echo "I'm going to replace '${search}' with '${replace}/Side Cutters.app/Contents/Resources/rsc/' in '${filename}'"
 echo "And I replace 'testpdf.pdf' with '${replace}/testpdf.pdf' in '${filename}'"
 if [[ $search != "" && $replace != "" ]]; then
-sed -i "" "s|${search}|${replace}/Side Cutters.app/Contents/Resources/rsc/|" $filename
-sed -i "" "s|testpdf.pdf|${replace}/testpdf.pdf|" $filename
+    sed -i "" "s|${search}|${replace}/Side Cutters.app/Contents/Resources/rsc/|" $filename
+    sed -i "" "s|testpdf.pdf|${replace}/testpdf.pdf|" $filename
 fi
-gcc src/main_.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
+cp $filename "Side Cutters.app/Contents/MacOS/config"
+gcc src/main.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
 chmod +x "Side Cutters.app"
 echo Run: Double click 'Side Cutters.app' - Apple MacOS application
 echo Press any key. 
