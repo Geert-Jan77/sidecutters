c32b9b8631ea2088858b2a1e3dae005c7a79cf0d
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-16 13:50:19 +0100

    Add files via upload

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -1,26 +1,51 @@
 # This bash script runs on MS Windows in an MSYS shell. 
 
-# Find the documents folder
-@echo off
-for /f "skip=2 tokens=2*" %%c in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do @set "Documents=%%d" && echo WIN XP - 10
-# xcopy "C:\test.txt" "%docs%" /f /y
-echo %docs%
+docs=$(reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" | grep Personal | awk '{print $3}')
+docs="${docs//\\//}"
+docs="${docs/C/c}"
+echo "Document folder ${docs}"
+desktop=$(reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" | grep Desktop | awk '{print $3}')
+desktop="${desktop//\\//}"
+desktop="${desktop/C/c}"
 
-Echo current users desktop
-for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop"') do @set "Desktop=%%d"
+echo "Desktop folder ${desktop}"
+pictures=$(reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" | grep 'My Pictures' | awk '{print $4}')
+pictures="${pictures//\\//}"
+pictures="${pictures/C/c}"  
 
-Echo current users documents
-for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do @set "Documents2=%%d"
+echo "Pictures ${pictures}"
+secondString="Downloads"
+downloads="${desktop/Desktop/"$secondString"}"  
+echo "Downloads ${downloads}"
+#wmic useraccount get sid | awk '{print $0}'
+sids=$(wmic useraccount get sid | awk '{print $0}')
+dollar="$"
+for f in $sids; 
+do 
+if [[ ${#f} -gt '40' ]]; then
+if [ -d "c:\\${dollar}Recycle.Bin\\${f}" ]; then 
+trash="c:\\${dollar}Recycle.Bin\\${f}"
+fi
+fi
+done
+trash="${trash//\\//}"
+echo "Trash ${trash}"
+filename="src/config_"
+cp "src/config" ${filename}
+sed -i "s|trashfolder|${trash}|" ${filename}
+workdir=$(pwd -W)
+workdir="${workdir//\\//}"
+workdir="${workdir/C/c}"
+workdir="${workdir/D/d}"
 
-Echo current user start menu
-for /f "skip=2 tokens=3*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Start Menu"') do @set "Start=%%d"
-
-Echo current user startup
-for /f "skip=2 tokens=2*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Startup"') do @set "Startup=%%d"
-
-Echo current users pictures
-for /f "skip=2 tokens=3*" %%c in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "My Pictures"') do @set "Pictures=%%d"
-
+sed -i "s|workingdirectory|${workdir}|" ${filename}
+if [ -d "c:/Program Files" ]; then sed -i "s|appfolder|c:/Program Files|" ${filename}
+fi
+if [ -d "${downloads}" ]; then sed -i "s|downloadfolder|${downloads}|" ${filename}
+fi
+sed -i "s|documentfolder|${docs}|" ${filename}
+sed -i "s|desktopfolder|${desktop}|" ${filename}
+sed -i "s|picturefolder|${pictures}|" ${filename}
 gcc --version | grep gcc | awk '{print $1" "$7}'
 gcc --version | awk '/gcc/ && ($7+0)==11.2{print "Version is correct"}'
 windres -i rsc/icon.rc -O coff -o rsc/icon.o
