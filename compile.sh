6c85cdb2708c329ff4f1650c302c2afaf57ec85a
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2021-12-21 11:17:19 +0100

    Delete compile.sh

--- compile.sh
+++
@@ -1,4 +0,0 @@
-gcc --version
-gcc src/main.c -o "Side Cutters.app/Contents/MacOS/Side-Cutters" `pkg-config --cflags --libs gtk+-3.0`
-chmod +x "Side Cutters.app"
-echo Run: Double click Side Cutters app
