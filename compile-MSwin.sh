ee9684f36672a2a95bdad52ff62a4d6d65582c96
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2022-01-02 07:25:37 +0100

    Add files via upload

--- compile-MSwin.sh
+++ compile-MSwin.sh
@@ -52,5 +52,20 @@ windres -i rsc/icon.rc -O coff -o rsc/icon.o
 windres -i rsc/info.rc -O coff -o rsc/info.o
 gcc src/main.c rsc/icon.o rsc/info.o -o "side cutters.exe" `pkg-config --cflags --libs gtk+-3.0`
 echo Run: Double click 'side cutters.exe' - MS Windows executable 
+apppath=$(pwd -W)
+appname="Side Cutters"
+shortcutdestinationpath=$startm
+shortcutdestinationpath="${shortcutdestinationpath///\\}"
+shortcutdestinationpath="${shortcutdestinationpath/c/C}"
+printf "Set oWS = WScript.CreateObject(\"WScript.Shell\")\n" > CreateShortcut.vbs
+echo "sLinkFile = \"${shortcutdestinationpath}/${appname}.lnk\"" >> CreateShortcut.vbs
+echo "Set oLink = oWS.CreateShortcut(sLinkFile)" >> CreateShortcut.vbs
+echo "oLink.TargetPath = \"${apppath}/${appname}.exe\"" >> CreateShortcut.vbs
+echo "oLink.WorkingDirectory = \"${apppath}\"" >> CreateShortcut.vbs
+echo "oLink.Description = \"${appname}\"" >> CreateShortcut.vbs
+echo "oLink.IconLocation = \"${apppath}/rsc/${appname}.ico\"" >> CreateShortcut.vbs
+echo "oLink.Save" >> CreateShortcut.vbs
+cscript -nologo CreateShortcut.vbs
+rm CreateShortcut.vbs
 echo Press any key. 
 read
